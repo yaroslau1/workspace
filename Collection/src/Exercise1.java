e69eff2f40783ad1a38b8fe619b42f6626697a01
yaroslau1 <samoilovich_y@Cardian.local>
2018-10-08 16:55:22 +0300

    Change spaces

--- Collection/src/Exercise1.java
+++ Collection/src/Exercise1.java
@@ -6,28 +6,26 @@ import java.util.TreeSet;
 public class Exercise1 {
 
 	public static void main(String[] args) {
-		
 
 		ArrayList<String> arr = new ArrayList<>();
-        Time.add(arr);
-        Time.search(arr);
-        Time.remove(arr);
-		
-       LinkedList <String> linl= new LinkedList<>();
-       Time.add(linl);
-       Time.search(linl);
-       Time.remove(linl);
-       
-		HashSet<String> has= new HashSet<>();
+		Time.add(arr);
+		Time.search(arr);
+		Time.remove(arr);
+
+		LinkedList<String> linl = new LinkedList<>();
+		Time.add(linl);
+		Time.search(linl);
+		Time.remove(linl);
+
+		HashSet<String> has = new HashSet<>();
 		Time.add(has);
 		Time.search(has);
 		Time.remove(has);
-		
-		TreeSet<String> tree=new TreeSet<>();
+
+		TreeSet<String> tree = new TreeSet<>();
 		Time.add(tree);
 		Time.search(tree);
 		Time.remove(tree);
-
 
 	}
 
--- FileExercise/src/Run.java
+++ FileExercise/src/Run.java
@@ -5,20 +5,17 @@ import java.io.IOException;
 public class Run {
 
 	public static void main(String[] args) {
-		
-	         
-	        try(FileInputStream fin=new FileInputStream("C:\\Users\\ira.NOO2012\\Desktop\\czKYBP81d4Q.jpg");
-	                FileOutputStream fos=new FileOutputStream("D:\\i\\programming.jpg"))
-	        {
-	            byte[] buffer = new byte[fin.available()];
-	            // считываем буфер
-	            fin.read(buffer, 0, buffer.length);
-	            // записываем из буфера в файл
-	            fos.write(buffer, 0, buffer.length);
-	        }
-	        catch(IOException ex){
-	             
-	            System.out.println(ex.getMessage());
-	        } 
+
+		try (FileInputStream fin = new FileInputStream("C:\\Users\\ira.NOO2012\\Desktop\\czKYBP81d4Q.jpg");
+				FileOutputStream fos = new FileOutputStream("D:\\i\\programming.jpg")) {
+			byte[] buffer = new byte[fin.available()];
+			// считываем буфер
+			fin.read(buffer, 0, buffer.length);
+			// записываем из буфера в файл
+			fos.write(buffer, 0, buffer.length);
+		} catch (IOException ex) {
+
+			System.out.println(ex.getMessage());
+		}
 	}
 }
