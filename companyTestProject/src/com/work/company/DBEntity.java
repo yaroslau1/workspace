07617aec980500c04970c07a8091aaab66b59a74
yaroslau1 <samoilovich_y@Cardian.local>
2018-10-17 18:28:46 +0300

    Not work
    Error: Unable to load authentication plugin 'caching_sha2_password'.

--- companyTestProject/src/com/work/company/DBEntity.java
+++ companyTestProject/src/com/work/company/DBEntity.java
@@ -1,5 +1,61 @@
 package com.work.company;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DBEntity {
+	
+	private String url;
+	private String user;
+	private String password;
+	private Connection connection;
+	private Statement statement;
+	private ResultSet resultSet;	
+	
+	public DBEntity(String url, String user, String password) {
+		this.url = url;
+		this.user = user;
+		this.password = password;
+		
+	}
+	
+	public void openConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");  	//динамическая загрузка
+			connection = DriverManager.getConnection(url, user, password);
+			statement = connection.createStatement();
+		} catch (SQLException | ClassNotFoundException e) {	
+			System.out.println("open");
+			System.out.println(e);
+		}
+		
+	}
+	
+	public void closeConnection() {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			System.out.println("close");
+			System.out.println(e);
+		}
+	}
+	
+	public void getAllInformationFromBD() {
+		try {
+			resultSet = statement.executeQuery("SELECT * FROM worker");
+			while(resultSet.next()){
+				String id = resultSet.getString("marka");
+				String name = resultSet.getString("speed");
+				String surname = resultSet.getString("price");
+				System.out.println(id + " "+ name + " " + surname);
+			}
+		} catch (SQLException e) {
+			System.out.println("get");
+			System.out.println(e);
+		}
+	}
 
 }
--- companyTestProject/src/com/work/company/MainAppCompany.java
+++ companyTestProject/src/com/work/company/MainAppCompany.java
@@ -3,6 +3,15 @@ package com.work.company;
 public class MainAppCompany {
 
 	public static void main(String[] args) {
+		String url = "jdbc:mysql://localhost/company";
+		String user = "root";
+		String password = "1234";
+		
+		DBEntity dbEntity = new DBEntity(url, user, password);
+		
+		dbEntity.openConnection();
+		dbEntity.getAllInformationFromBD();
+		dbEntity.closeConnection();
 
 	}
 
