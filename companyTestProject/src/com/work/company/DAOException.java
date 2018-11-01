e2ef3a84eaac299653b7677c8ddd98ef8b281199
yaroslau1 <samoilovich_y@Cardian.local>
2018-11-01 09:40:49 +0300

    connect to BD with DAO v0.6

--- companyTestProject/src/com/work/company/DAOException.java
+++ companyTestProject/src/com/work/company/DAOException.java
@@ -7,27 +7,22 @@ public class DAOException extends Exception {
 	
 	public DAOException(SQLException e) {
 		initCause(e);
-		System.out.println("Something wrong with SQL query");
 	}
 	
 	public DAOException(InstantiationException e) {
 		initCause(e);
-		System.out.println("Something wromg with Class object");
 	}
 	
 	public DAOException(IllegalAccessException e) {
 		initCause(e);
-		System.out.println("Something wromg with access to DB");
 	}
 	
 	public DAOException(ClassNotFoundException e) {
 		initCause(e);
-		System.out.println("Something wromg with ClassPath");
 	}
 	
 	public DAOException(IOException e) {
 		initCause(e);
-		System.out.println("Something wromg with file opening");
 	}
 
 }
